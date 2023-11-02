1bc5f22242c15126b0ca29eec87b4d5fd7e68172
Stargazer3240 <112516550+Stargazer3240@users.noreply.github.com>
2023-11-01 22:48:29 -0300

    Added neotree and treesitter context bindings

--- lua/fabricio/plugins.lua
+++ lua/fabricio/plugins.lua
@@ -177,4 +177,19 @@ return {
       vim.fn["mkdp#util#install"]()
     end,
   },
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      filesystem = {
+        hijack_netrw_behavior = "open_current",
+      },
+    },
+  },
 }
